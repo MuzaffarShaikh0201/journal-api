e8e1082d5131f001673587ddf0d1990fb7b95bd2
MuzaffarShaikh0201 <114210961+MuzaffarShaikh0201@users.noreply.github.com>
2025-01-15 05:25:28 +0530

    Added Dockerfile

--- src/run.py
+++ src/run.py
@@ -4,7 +4,3 @@ from .main import app
 
 def run():
     uvicorn.run(app, host="0.0.0.0", port=5000, use_colors=True)
-
-
-def dev():
-    uvicorn.run(app="src.main:app", port=5000, reload=True, use_colors=True)
